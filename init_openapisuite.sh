d0ea0f92bbbe28e780a032c82071106be857501b
Lorenzo Scalese <lorenzo.scalese@gmail.com>
2023-02-06 21:13:11 +0100

    Add swagger validator and swagger converter services available locally

---
+++ init_openapisuite.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+openapi_suite() {
+iris session $ISC_PACKAGE_INSTANCENAME -U IRISAPP <<- END
+Set ^swaggerconverter("ConverterURL") = "${CONVERTER_URL:-converter.swagger.io}"
+Set ^swaggerconverter("Port") = "${CONVERTER_PORT:-80}"
+Set ^swaggervalidator("ValidatorURL") = "${VALIDATOR_URL:-validator.swagger.io}"
+Set ^swaggervalidator("Port") = "${VALIDATOR_PORT:-80}"
+Halt
+END
+}
+
+openapi_suite
+
+exit 0
